442b96e3798cc0c5bce5828b95fbc08b9d331a03
Rose215 <77243703+Rose215@users.noreply.github.com>
2021-01-10 13:55:04 -0500

    Update Git.java

--- src/demo/Git.java
+++ src/demo/Git.java
@@ -4,7 +4,8 @@ public class Git {
 	public static void main(String[] args) {
 		System.out.println("hello world");
 		System.out.println("hello git");
-		
+		//writting 3rd print
+		System.out.println("hello2 ");
 	}
 
 }
